518bc39f73e1093f40114ac475ebb4e09ba75cdb
aaditya1703 <34514731+aaditya1703@users.noreply.github.com>
2020-08-05 15:08:44 +0530

    division module added

--- FirstFile.java
+++ FirstFile.java
@@ -3,6 +3,9 @@ Class Calculator{
     System.out.println("Sum = "+(a+b));
   }
   public void add(int a, int b){
-    System.out.println("Sum = "+(a-b));
+    System.out.println("Diff = "+(a-b));
+    }
+    public void div(int a, int b){
+    System.out.println("Quotient = "+(a/b));
     }
 }
