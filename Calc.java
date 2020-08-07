7a85bc6188c3259c8a557c94c2d0a76ad6456e20
aaditya1703 <34514731+aaditya1703@users.noreply.github.com>
2020-08-07 17:24:48 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -2,25 +2,25 @@ package com.lti;
 
 public class Calc {
 	
-	public static void add(int a, int b) {
+	public static int add(int a, int b) {
 		System.out.println("Sum="+(a+b));
 	}
 	
-	public static void diff(int a, int b) {
+	public static int diff(int a, int b) {
 		System.out.println("Diff="+(a-b));
 	}
-	public static void product(int a, int b) {
+	public static int product(int a, int b) {
 		System.out.println("Product="+(a*b));
 	}
-	public static void division(int a, int b) {
+	public static int division(int a, int b) {
 		System.out.println("Quotient="+(a/b));
 	}
 
 	public static void main(String[] args) {
 		    System.out.println(add(10,20));
-		    System.out.println(sub(10,20));
-		    System.out.println(mul(10,20));
-		    System.out.println(div(10,20));    
+		    System.out.println(diff(10,20));
+		    System.out.println(product(10,20));
+		    System.out.println(division(10,20));    
 
 	}
 
