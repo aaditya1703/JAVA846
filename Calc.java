8fcad5cb72d3721bf1aeb6772c94e056748ddbf8
aaditya1703 <34514731+aaditya1703@users.noreply.github.com>
2020-08-07 17:54:00 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -10,15 +10,15 @@ public class Calc{
 	public static int product(int a, int b) {
 		return (a*b);
 	}
-	public static int division(int a, int b) {
-		return (a/b);
-	}
+// 	public static int division(int a, int b) {
+// 		return (a/b);
+// 	}
 
 	public static void main(String[] args) {
 		    System.out.println(add(10,20));
 		    System.out.println(diff(10,20));
 		    System.out.println(product(10,20));
-		    System.out.println(division(10,20));    
+// 		    System.out.println(division(10,20));    
 
 	}
 
